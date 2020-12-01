403eb09260388b71b3ddb8972b1c1e20a0718036
Azat Akhmetyanov <a.r.akhmetyanov@gmail.com>
2020-12-01 06:10:23 +0300

    Reformat test file

--- tests/test_dbql_grammar.py
+++ tests/test_dbql_grammar.py
@@ -3,91 +3,91 @@ from src.dbql_lexer import get_lex
 import pytest
 @pytest.mark.parametrize( "grammar", [GrammarCNF.from_txt( "dbql_grammar.txt" )] )
 @pytest.mark.parametrize( "test_input, expected", [
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from query term "s" star alt term "b" plus concat term "c" opt ;
-        ''', 
+        ''',
         True
-    ), 
-    ( 
+    ),
+    (
         '''
         select edges from name "sparsegraph" ;
-        ''', 
+        ''',
         True
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
-        ''', 
+        ''',
         True
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges from name "sparsegraph_256.txt" ;
-        ''', 
+        ''',
         True
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
             select edges 
             from startAndFinal ( set 1 2 3, set 4 5 6 )
             of name "sparsegraph" ;
-        ''', 
+        ''',
         True
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from startAndFinal ( range ( 1, 3 ), set 4 5 6 ) of name "sparsegraph" ;
-        ''', 
+        ''',
         True
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select filter edges with 
             ( u, l, v ) satisfies isStart u and isFinal v
                     from name "sparsegraph" ;
-        ''', 
+        ''',
         True
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select filter edges with 
             ( u, l, v ) satisfies l labelIs "ar" or ( isStart u and isFinal v ) 
                     from name "sparsegraph" ;
-        ''', 
+        ''',
         True
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select filter edges with 
             ( u, l, v ) satisfies l labelIs "ar" and isStart u 
                     from name "sparsegraph.txt" ;
-        ''', 
+        ''',
         True
-    ),   
-    ( 
+    ),
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from query term "a" star alt ( term "b" opt concat term "c" plus ) ;
-        ''', 
+        ''',
         True
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from name "sparsegraph" intersect query term "a" alt term "b" ;
-        ''', 
+        ''',
         True
     ),
     # graph expression with multiple levels:
@@ -101,17 +101,17 @@ import pytest
         True
     ),
     # edge expressions with multiple levels:
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
 
         select count filter edges 
         with ( u, e, v ) satisfies not isStart u and isFinal v
             from name "worstcase" ;
-        ''', 
+        ''',
         True
     ),
-     ( 
+     (
         '''
         connect "azat/home/db" ; 
         define 
@@ -122,10 +122,10 @@ import pytest
         as "s1" ;
         select edges 
             from name "sparsegraph256.txt" ;
-        ''', 
+        ''',
         True
-    ),   
-    ( 
+    ),
+    (
         '''
         connect "azat/home/db" ; 
         define 
@@ -134,61 +134,61 @@ import pytest
         select edges 
             from name "sparsegraph" 
                 intersect query term "a" alt term "b" ;
-        ''', 
+        ''',
         True
     ),
     # the rest are False test cases ( when grammar shouldn't accept )
     # mismatched brackets in pattern:
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from query term "a" star alt ( term "a" opt concat term "c" plus ;
-        ''', 
+        ''',
         False
     ),
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from query term "a" star alt ( term "a" opt concat term "c" plus ))) ;
-        ''', 
+        ''',
         False
     ),
     # wrong data type in range:
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from startAndFinal ( range ( "typo", 3 ), set 4 5 6 ) of name "sparsegraph" ;
-        ''', 
+        ''',
         False
     ),
     # wrong data type in set:
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from startAndFinal ( range ( 1, 3 ), set typo 5 6 ) of name "sparsegraph" ;
-        ''', 
+        ''',
         False
     ),
     # not specified term or var in pattern:
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from query "a" star alt "a" opt concat "c" plus ;
-        ''', 
+        ''',
         False
      ),
     # string includes special character +
-    ( 
+    (
         '''
         connect "azat/home/db" ; 
         select edges 
             from query "a+" star alt "a" opt concat "c" plus ;
-        ''', 
+        ''',
         False
      )
 ] )
